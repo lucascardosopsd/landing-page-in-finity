823595e6672b139f6d7beb84a11bbc1b39a59c6a
Lucas Cardoso <lucascardosopsd@gmail.com>
2025-02-09 12:31:27 -0300

    fix: citation section overflow

--- src/components/sections/Citation.tsx
+++ src/components/sections/Citation.tsx
@@ -2,7 +2,7 @@ import TextTyping from "../TextTyping";
 
 const Citation = () => {
   return (
-    <div className="h-[40svh] tablet:h-40  w-full flex flex-col items-center justify-center bg-red-500 text-background p-10 sticky top-0">
+    <div className="h-[40svh] tablet:h-40  w-full flex flex-col items-center justify-center bg-red-500 text-background p-10 sticky top-0 overflow-hidden">
       <div className="relative">
         <p className="text-4xl font-bold absolute -left-5">"</p>
 
