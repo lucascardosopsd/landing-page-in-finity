5646850f9c75a50c8fe26225bac5d8a527ca1835
Lucas Cardoso <lucascardosopsd@gmail.com>
2025-02-12 12:22:56 -0300

    update: hero section

--- src/components/sections/Hero.tsx
+++ src/components/sections/Hero.tsx
@@ -5,14 +5,14 @@ import { HighlightWords } from "../HighlightWords";
 import { Button } from "../ui/button";
 
 import { motion } from "motion/react";
+import Link from "next/link";
 
 const HeroSection = () => {
   return (
-    <div className="h-[calc(100svh-5rem)] py-5 flex items-center justify-center relative bg-blue-950">
-      <div className="hidden tablet:block bg-light-radial from-blue-100 via-transparent to-transparent h-[2000px] w-[2000px] absolute -right-[125%] -top-[150%] z-50" />
-
-      <div className="mx-auto max-w-screen-desktop px-10">
-        <div className="flex items-center text-4xl tablet:text-5xl">
+    <div className="min-h-[calc(100svh-5rem)] flex items-center justify-center relative bg-blue-950">
+      {/* Text Section */}
+      <div className="mx-auto max-w-screen-desktop p-10 py-20">
+        <div className="flex flex-col gap-10 tablet:gap-0 tablet:flex-row tablet:items-center text-4xl tablet:text-5xl">
           <div className="flex flex-col flex-1 justify-start text-background gap-4">
             <motion.div initial={{ y: 50 }} whileInView={{ y: 0 }}>
               <HighlightWords
@@ -24,15 +24,20 @@ const HeroSection = () => {
               </HighlightWords>
             </motion.div>
 
-            <Button className="p-5 w-full bg-red-500 text-blue-950 hover:text-background font-bold text-xl tablet:text-2xl desktop:text-4xl uppercase">
-              <motion.div
-                initial={{ y: 50 }}
-                whileInView={{ y: 0 }}
-                transition={{ delay: 0.4 }}
-              >
-                Mais informações
-              </motion.div>
-            </Button>
+            <Link
+              href="https://api.whatsapp.com/send?phone=5517997122513"
+              className="z-50"
+            >
+              <Button className="p-5 w-full bg-red-500 text-blue-950 hover:text-background font-bold text-xl tablet:text-2xl desktop:text-3xl uppercase">
+                <motion.div
+                  initial={{ y: 50 }}
+                  whileInView={{ y: 0 }}
+                  transition={{ delay: 0.4 }}
+                >
+                  Mais informações
+                </motion.div>
+              </Button>
+            </Link>
 
             <div className="relative">
               <motion.div
@@ -54,28 +59,31 @@ const HeroSection = () => {
             </div>
           </div>
 
-          <div className="flex-1 items-center justify-center w-full h-full hidden tablet:flex relative">
+          <div className="flex-1 items-center justify-center w-full h-full relative">
             <motion.div
               initial={{ y: 50, opacity: 0 }}
               whileInView={{ y: 0, opacity: 1 }}
               transition={{ delay: 0.4, duration: 0.5 }}
-              className="absolute bottom-0 z-40 "
+              className="absolute bottom-0 z-40 right-0 left-0 mx-auto"
             >
-              <p className="text-background text-base">Luide Mendes - CEO</p>
+              <p className="text-background text-base w-full text-center">
+                Luide Mendes - CEO
+              </p>
             </motion.div>
 
             <div className="h-full w-full max-h-[400px] bg-gradient-to-t from-blue-950 to-transparent z-20 absolute bottom-0 left-0" />
+
             <motion.div
               initial={{ opacity: 0 }}
               whileInView={{ opacity: 1 }}
               transition={{ delay: 0.4, duration: 0.5 }}
             >
               <Image
-                alt="man"
+                alt="luide Amorin"
                 src="/luide-site.png"
                 height={1000}
                 width={1000}
-                className="h-full w-auto max-h-[70svh]"
+                className="h-full w-auto max-h-[70svh] mx-auto"
               />
             </motion.div>
           </div>
