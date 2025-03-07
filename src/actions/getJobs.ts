49ce61ab5a93a229801bc27419275904278af9be
lucas cardoso <lucascardosopsd@gmail.com>
2025-03-07 16:51:34 -0300

    action: get jobs

---
+++ src/actions/getJobs.ts
@@ -0,0 +1,23 @@
+import { payload } from "@/lib/payload";
+
+type JobProps = {
+  id: string;
+  role: string;
+  company: {
+    id: string;
+    logo: string;
+    name: string;
+    email: string;
+    legalName: string;
+    cnpj: string;
+    activityArea: string;
+  };
+  description: string;
+  active: string;
+};
+
+export const getJobs = async (): Promise<JobProps[]> => {
+  return (await payload.find({
+    collection: "jobs",
+  })) as unknown as JobProps[];
+};
